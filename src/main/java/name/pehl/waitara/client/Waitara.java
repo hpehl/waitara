9233d609b3773dbbabfea6bcac66153c50c2bae0
Harald Pehl <harald.pehl@gmail.com>
2011-06-14 14:00:58 +0000

    Initial import

---
+++ src/main/java/name/pehl/waitara/client/Waitara.java
@@ -0,0 +1,15 @@
+package name.pehl.tire.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author $Author: harald.pehl $
+ * @version $Revision: 95 $
+ */
+public class Waitara implements EntryPoint
+{
+    @Override
+    public void onModuleLoad()
+    {
+    }
+}
